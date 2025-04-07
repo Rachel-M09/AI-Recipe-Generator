ffb331d6abc60102d8ebbc0bda04c893f5459e12
Rachel-M09 <129060558+Rachel-M09@users.noreply.github.com>
2025-04-07 16:16:54 +0200

    Updated the API and JS for user experience

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function displayRecipe(response) {
-    new Typewriter("#search-results", {
+    new Typewriter("#recipe", {
         strings: response.data.answer,
         autoStart: true,
         delay: 1,
@@ -16,6 +16,8 @@ function generateRecipe(event){
     let prompt = `generate a delicious recipe about ${searchInput.value}`;
     let context = "You are a well-renowned culinary expert, an amazing chef and you know all delicious recipes in the world. Make sure that the recipe is easy to follow and the food is delicious. Provide only the recipe and no additional talk. Provide the recipe in a way that is easy to read. The ingredients content should be on a seperate paragraph from the instructions content. Be attentive to what the user is searching for.";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
 
 
     console.log(`prompt: ${prompt}`);
